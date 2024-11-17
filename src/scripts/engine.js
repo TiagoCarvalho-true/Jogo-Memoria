c8ff0eb10c30829d853dc6a1b62465b97250e97b
TiagoCarvalho-true <carvalhost58@gmail.com>
2024-11-16 23:17:56 -0400

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,48 @@
+const emojis =[
+    "🤡",
+    "🤡",
+    "💀",
+    "💀",
+    "🤓",
+    "🤓",
+    "😍",
+    "😍",
+    "😈",
+    "😈",
+    "👀",
+    "👀"
+];
+let openCards= [];
+let suflleEmojis= emojis.sort(()=>(Math.random()>0.5 ? 2:-1));
+for(let i=0;i<emojis.length;i++){
+   let box=document.createElement("div");
+   box.className="item";
+   box.innerHTML=emojis[i];
+   box.onclick=handleClick;
+   document.querySelector(".game").appendChild(box);
+}
+function handleClick()
+{
+   if(openCards.length<2){
+    this.classList.add("boxOpen");
+    openCards.push(this);
+    }
+    if(openCards.length==2){
+        setTimeout(checkMatch,500)
+    }
+}
+
+function checkMatch(){
+   if(openCards[0].innerHTML===openCards[1].innerHTML){
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+   }else{
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+   }
+   openCards=[];
+    if(document.querySelectorAll(".boxMatch").length===emojis.length){
+        document.querySelector(".wins").innerHTML="🎉Voce venceu, Parabens🎉";
+        alert("🎉Voce venceu, Parabens🎉")
+    }
+}
